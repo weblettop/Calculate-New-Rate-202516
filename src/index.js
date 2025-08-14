def1a79591e787a9b2aadba6307c6913acde58ad
weblettop <weblettop.gmail.com>
2025-08-14 21:20:59 +0300

    Added bug fixes in task 14082025

--- src/index.js
+++ src/index.js
@@ -4,25 +4,28 @@
  * Function for calculation of athlete rating based on the winner and opponent ratings.
  * @param {number} winner - Rating of the winner
  * @param {number} opponent - Rating of the opponent
- * @returns {number} The new rating rounded to tenths 
+ * @returns {number} The new rating rounded to tenths
  * @description Calculates the new rating of the winner based on rating difference and bonus rules.
  *
  */
 
 function rating(winner, opponent) {
   if (typeof winner !== 'number' || typeof opponent !== 'number') {
-    throw('Error: Wrong data type') 
+    throw 'Error: Wrong data type'
   }
 
   let ratingDifference = winner - opponent
+
   if (isNaN(ratingDifference)) {
-    throw('Error: It is not NaN')
+    throw 'Error: It is not NaN'
   }
   if (winner < 0 || opponent < 0) {
-    throw("Error:The rating can't be less than 0")
+    throw "Error:The rating can't be less than 0"
   }
-
-  if (ratingDifference >= 0 && ratingDifference <= 2) {
+  if (winner === 0) {
+    return +parseFloat(opponent).toFixed(1)
+  }
+  if (ratingDifference > 0 && ratingDifference <= 2) {
     return parseFloat((winner + 2).toFixed(1))
   }
   if (ratingDifference > 2 && ratingDifference < 20) {
@@ -36,9 +39,11 @@ function rating(winner, opponent) {
       (winner + (Math.abs(ratingDifference) + 5) / 3).toFixed(1)
     )
   }
+
   return parseFloat(winner.toFixed(1))
 }
-console.log(rating(1, 1))
+console.log(rating(1, 0))
+console.log(rating(0, 20))
 console.log(rating(2.1, 0))
 console.log(rating(4, 2.1))
 console.log(rating(22, 2))
